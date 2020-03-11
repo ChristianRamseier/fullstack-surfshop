5f8f1050fef5a146058339570d58a393619e1b75
Christian Ramseier <chrigi@gmail.com>
2020-03-11 15:56:37 +0100

    Remove weather user interface

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { InventoryComponent } from './inventory/inventory.component';
-import { WeatherComponent } from './weather/weather.component';
 import { CustomerComponent } from './customer/customer.component';
 import { ReservationComponent } from './reservation/reservation.component';
 import { LayoutComponent } from './commons/layout/layout.component';
 import { ButtonComponent } from './commons/button/button.component';
 import { NavigationComponent } from './navigation/navigation.component';
-import {ReservationService} from "./reservation/service/reservation.service";
-import {HttpClientModule} from "@angular/common/http";
+import {HttpClientModule} from '@angular/common/http';
 import { WindDirectionPipe } from './commons/wind-direction/wind-direction.pipe';
 
 @NgModule({
@@ -20,7 +18,6 @@ import { WindDirectionPipe } from './commons/wind-direction/wind-direction.pipe'
     AppComponent,
     DashboardComponent,
     InventoryComponent,
-    WeatherComponent,
     CustomerComponent,
     ReservationComponent,
     LayoutComponent,
